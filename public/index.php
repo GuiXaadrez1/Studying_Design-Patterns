407050d5b1630293b0f402ac797922300485593e
Guixaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2026-08-29 22:14:17 -0300

    Estruturando o ambiente de desenvolvimento para estudos e reconfigurando o httpd-vhost.conf para simular de forma real um ambiente de desenvolimento com apache para ficar confortavel trabalhando no meu pc de casa que nao tem como configurao arquivo host  em Windows/system32/drivers/extra/host e para isso eu  configurei o index.phpdo htdocs para simular o localhost e apontar para a pasta public deste projeto

--- public/index.php
+++ public/index.php
@@ -4,16 +4,18 @@
 	//echo "Hellow Word<br>";
 
 
-	$url = $_GET['url'] ?? '';
+	$url = $_GET['rota'] ?? '';
 
 
-	echo 'URI/URL COMPLETA: ' . $_SERVER['HTTP_HOST'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'] . '<br>';
+	echo 'URI/URL COMPLETA: ' . $_SERVER['HTTP_HOST'] .'|'. $_SERVER['REMOTE_ADDR'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'] . '<br>';
 	#echo . $url;
 
 	# aqui estou construindo dinamicamente a URI para determiando serviço
 	#echo $_SERVER['HTTP_HOST'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
 
 	echo "<br>";
-	echo "PARABENS... VOCE CHEGOU ATÉ AQUI!"
+	echo "PARABENS... VOCE CHEGOU ATÉ AQUI!";
+	echo "<br>";
 
+	echo $url;
 ?>	
